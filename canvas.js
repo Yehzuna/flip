f4eac168926668c1fd20742c8b701ef4664140da
Yehzuna <yehzuna@outlook.com>
2017-11-03 20:04:24 +0100

    Canvas dev

--- canvas.js
+++ canvas.js
@@ -1,19 +1,18 @@
 function getRandomColor() {
     let o = Math.round, r = Math.random, s = 255;
-    return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
+    return 'rgb(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ')';
 }
 
+const fw = window.innerWidth;
+const fh = window.innerHeight;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const color = getRandomColor();
+ctx.canvas.width = fw;
+ctx.canvas.height = fh;
+
+let data = {};
 let raf;
 let obj;
-
-/*
-canvas.addEventListener('click', function(event) {
-    draw();
-});
-*/
 
 let ball = {
     x: 0,
@@ -26,14 +25,14 @@ let ball = {
     direction: true,
     color: getRandomColor(),
     raf: null,
-    draw: function() {
+    draw: function () {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     },
-    clear: function() {
+    clear: function () {
         ctx.clearRect(this.xmax, this.ymax, this.max, this.max);
     },
-    reset: function() {
+    reset: function () {
         ctx.fillRect(this.xmax, this.ymax, this.max, this.max);
     },
 };
@@ -62,115 +61,141 @@ function flip() {
     raf = window.requestAnimationFrame(flip);
 }
 
-
-let multi = {};
 function init() {
     for (let i = 0; i <= 10; i++) {
-        ball.xmax = i*ball.max;
-        ball.x = i*ball.max;
+        ball.xmax = i * ball.max;
+        ball.x = i * ball.max;
 
-        multi[i] = Object.assign({}, ball);
-        multi[i].draw();
+        data[i] = Object.assign({}, ball);
+        data[i].draw();
     }
-    console.log(multi);
+    console.log(data);
 }
 
 init();
 
-
-canvas.addEventListener('mousemove', function(e) {
-    console.log(e);
-
-
-    obj = multi[0];
-    raf = window.requestAnimationFrame(flip);
-});
-
-canvas.addEventListener('mouseout', function(e) {
+canvas.addEventListener('mouseout', function (e) {
     obj.clear();
     obj.reset();
+    obj = null;
     window.cancelAnimationFrame(raf);
 });
+
+let hasScroll = false;
+let event = false;
+canvas.addEventListener('mousemove', function (e) {
+    hasScroll = true;
+    event = e;
+});
+
+setInterval(function () {
+    if (hasScroll) {
+        animate(event);
+        hasScroll = false;
+    }
+}, 100);
+
+
+function animate(e) {
+    const x = Math.round(e.x / ball.max);
+    console.log(x);
+
+    if (obj !== data[x]) {
+        console.log(e);
+
+        if (obj) {
+            obj.clear();
+            obj.reset();
+            obj = null;
+        }
+
+        if (data[x]) {
+            obj = data[x];
+            raf = window.requestAnimationFrame(flip);
+        }
+    }
+}
+
 
 //ball.draw();
 
 /*
-function draw() {
-    const dim = 80;
-    const fh = window.innerHeight;
-    const fw = window.innerWidth;
+ function draw() {
+ const dim = 80;
+ const fh = window.innerHeight;
+ const fw = window.innerWidth;
 
-    const h = Math.floor(fh / dim);
-    const w = Math.floor(fw / dim);
+ const h = Math.floor(fh / dim);
+ const w = Math.floor(fw / dim);
 
-    console.log(fh, fw, color);
+ console.log(fh, fw, color);
 
-    ctx.fillStyle = getRandomColor();
-    ctx.rotate(90 * Math.PI / 180);
-    ctx.fillRect(0, 0, 800, 600);
+ ctx.fillStyle = getRandomColor();
+ ctx.rotate(90 * Math.PI / 180);
+ ctx.fillRect(0, 0, 800, 600);
 
-    //ctx.fillStyle = getRandomColor();
-    //ctx.rotate(-90);
-}
+ //ctx.fillStyle = getRandomColor();
+ //ctx.rotate(-90);
+ }
 
-draw();
-*/
+ draw();
+ */
 
 /*
-function getRandomColor() {
-    let o = Math.round, r = Math.random, s = 255;
-    return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
-}
+ function getRandomColor() {
+ let o = Math.round, r = Math.random, s = 255;
+ return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
+ }
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const color = getRandomColor();
-let raf;
+ const canvas = document.getElementById('canvas');
+ const ctx = canvas.getContext('2d');
+ const color = getRandomColor();
+ let raf;
 
-let ball = {
-    x: 100,
-    y: 100,
-    h: 48,
-    w: 48,
-    max: 48,
-    direction: true,
-    color: getRandomColor(),
-    draw: function() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
-};
+ let ball = {
+ x: 100,
+ y: 100,
+ h: 48,
+ w: 48,
+ max: 48,
+ direction: true,
+ color: getRandomColor(),
+ draw: function() {
+ ctx.fillStyle = this.color;
+ ctx.fillRect(this.x, this.y, this.w, this.h);
+ }
+ };
 
-function draw() {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    ball.draw();
+ function draw() {
+ ctx.clearRect(0,0, canvas.width, canvas.height);
+ ball.draw();
 
-    if (ball.direction) {
-        ball.x += 3;
-        ball.w -= 6;
-    } else {
-        ball.x -= 3;
-        ball.w += 6;
-    }
+ if (ball.direction) {
+ ball.x += 3;
+ ball.w -= 6;
+ } else {
+ ball.x -= 3;
+ ball.w += 6;
+ }
 
-    if (ball.w <= 0) {
-        ball.color = getRandomColor();
-        ball.direction = false;
-    }
+ if (ball.w <= 0) {
+ ball.color = getRandomColor();
+ ball.direction = false;
+ }
 
-    if (ball.w >= ball.max) {
-        ball.direction = true;
-    }
-    raf = window.requestAnimationFrame(draw);
-}
+ if (ball.w >= ball.max) {
+ ball.direction = true;
+ }
+ raf = window.requestAnimationFrame(draw);
+ }
 
-canvas.addEventListener('mouseover', function(e) {
-    raf = window.requestAnimationFrame(draw);
-});
+ canvas.addEventListener('mouseover', function(e) {
+ raf = window.requestAnimationFrame(draw);
+ });
 
-canvas.addEventListener('mouseout', function(e) {
-    window.cancelAnimationFrame(raf);
-});
+ canvas.addEventListener('mouseout', function(e) {
+ window.cancelAnimationFrame(raf);
+ });
 
-ball.draw();
-*/
+ ball.draw();
+ */
